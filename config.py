90b552878facd4ccb0b34d1b85b7e65d1008f2a8
Slava <vvb01@mail.ru>
2021-11-07 20:42:59 +0300

    Update config.py
    simplify config

--- config.py
+++ config.py
@@ -1,12 +1,5 @@
 root_path = "./"
 
-metall_name = {
-        'A33':'Palladium',
-        'A76':'Platinum',
-        'A98':'Gold',
-        'A99':'Silver'
-}
-
 #TODO  error with drawing. top and bottom are mixedup instead of scaling chart is just shifting. 
 #workaround: Use equal numbers by now.
 margin_l = 5
@@ -24,8 +17,6 @@ text_color = (0,0,0,255)  #black, non-transparent
 
 chart_pen_width = 2
 
-#chart_font_type = "./font/arial.ttf"
-#chart_font_type = "./font/Guru-Regular.otf"
-chart_font_type = root_path + "font/GothamSSm-Book.otf"
+chart_font_type = root_path + "GothamSSm-Book.otf"
 chart_font_size = 16
 
